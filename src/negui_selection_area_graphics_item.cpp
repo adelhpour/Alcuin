bdf0ebe6d34e184fa1ae440bef18af308c9ccca6
adelhpour <ad.heydarabadi@gmail.com>
2023-03-28 19:34:56 -0700

    the pen and brush color of the selection area graphics item is set

--- src/negui_selection_area_graphics_item.cpp
+++ src/negui_selection_area_graphics_item.cpp
@@ -5,8 +5,18 @@
 MySelectionAreaGraphicsItem::MySelectionAreaGraphicsItem(const QPointF& initialPosition, QGraphicsItem *parent) : QGraphicsRectItem(parent) {
     _initialPosition = initialPosition;
     setRect(_initialPosition.x(), _initialPosition.y(), 0.0, 0.0);
+    setPen(defaultPen());
+    setBrush(defaultBrush());
 }
 
 void MySelectionAreaGraphicsItem::updateExtents(const QPointF& position) {
     setRect(qMin(_initialPosition.x(), position.x()), qMin(_initialPosition.y(), position.y()), qAbs(position.x() - _initialPosition.x()), qAbs(position.y() - _initialPosition.y()));
+}
+
+const QPen MySelectionAreaGraphicsItem::defaultPen() {
+    return QPen(QColor("#4169e1"));
+}
+
+const QBrush MySelectionAreaGraphicsItem::defaultBrush() {
+    return QBrush(QColor("#87CEEB"), Qt::SolidPattern);
 }
--- src/negui_selection_area_graphics_item.h
+++ src/negui_selection_area_graphics_item.h
@@ -2,6 +2,8 @@
 #define __NEGUI_SELECTION_AREA_GRAPHICS_ITEM_H
 
 #include <QGraphicsRectItem>
+#include <QPen>
+#include <QBrush>
 
 class MySelectionAreaGraphicsItem : public QGraphicsRectItem {
 
@@ -9,6 +11,10 @@ public:
     MySelectionAreaGraphicsItem(const QPointF& initialPosition, QGraphicsItem *parent=nullptr);
 
     void updateExtents(const QPointF& position);
+
+    const QPen defaultPen();
+
+    const QBrush defaultBrush();
 
 protected:
     QPointF _initialPosition;
