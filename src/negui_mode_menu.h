0a4e14bb071c1cb9c68a2da10fc6e59f495ca897
adelhpour <>
2023-05-15 17:59:21 -0700

    common widgets is included in mode menu

--- src/negui_mode_menu.h
+++ src/negui_mode_menu.h
@@ -1,7 +1,7 @@
 #ifndef __NEGUI_MODE_MENU_H
 #define __NEGUI_MODE_MENU_H
 
-#include "negui_parameters.h"
+#include "negui_customized_common_widgets.h"
 
 class MyModeMenu : public MyFrame {
     Q_OBJECT
