1af78acc05372b300ac6ed2a824703a95064a094
adelhpour <>
2023-05-15 18:42:29 -0700

    mode menu layout is added

--- src/negui_mode_menu.cpp
+++ src/negui_mode_menu.cpp
@@ -1,7 +1,11 @@
 #include "negui_mode_menu.h"
 
+#include <QGridLayout>
+
 // MyModeMenu
 
 MyModeMenu::MyModeMenu(QWidget *parent) : MyFrame(parent) {
-
+    QGridLayout* contentLayout = new QGridLayout(this);
+    contentLayout->addWidget(new MyLabel("Mode"), 0, 0);
+    setLayout(contentLayout);
 }
