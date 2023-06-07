46ab34f2482fa47d54eed394097ea42939377701
adelhpour <>
2023-06-07 10:48:43 -0700

    network element create right click menu signal is created and connected

--- src/negui_network_element_base.cpp
+++ src/negui_network_element_base.cpp
@@ -22,6 +22,7 @@ void MyNetworkElementBase::updateGraphicsItem() {
 void MyNetworkElementBase::connectGraphicsItem() {
     connect(_graphicsItem, &MyNetworkElementGraphicsItemBase::mouseLeftButtonIsPressed, this, [this] () { emit elementObject(this); });
     connect(_graphicsItem, SIGNAL(mouseLeftButtonIsDoubleClicked()), this, SLOT(createFeatureMenu()));
+    connect(_graphicsItem, SIGNAL(mouseRightButtonIsReleased()), this, SLOT(createRightClickMenu()));
     connect(_graphicsItem, SIGNAL(askForCreateChangeStageCommand()), this, SIGNAL(askForCreateChangeStageCommand()));
 }
 
@@ -114,5 +115,11 @@ void MyNetworkElementBase::createFeatureMenu() {
             updateGraphicsItem();
             emit askForCreateChangeStageCommand(); } );
         emit askForDisplayFeatureMenu(featureMenu);
+    }
+}
+
+void MyNetworkElementBase::createRightClickMenu() {
+    if (getSceneMode() == NORMAL_MODE) {
+
     }
 }
