83da3160595f066576419ae8ec8ef799cfadd047
adelhpour <>
2023-07-31 11:46:17 -0700

    using the sbmlplot functions in their new form

--- test-app/src/plugins/negui_data_export_plugin.py
+++ test-app/src/plugins/negui_data_export_plugin.py
@@ -104,9 +104,9 @@ def writeJSON(graphInfo, filename):
         json.dump(graphInfo, js_file, indent=1)
 
 
-def writeSBML(graphInfo, file_name):
-    sbml_graph_info = sbmlplot.SBMLGraphInfoImportFromNetworkEditor()
-    sbml_graph_info.extract_info(graphInfo)
-    sbml_export = sbmlplot.SBMLGraphInfoExportToSBMLModel()
-    sbml_export.extract_graph_info(sbml_graph_info)
-    sbml_export.export(file_name)
+def writeSBML(network_info, file_name):
+    network_info_import_from_network_editor = sbmlplot.NetworkInfoImportFromNetworkEditor()
+    network_info_import_from_network_editor.extract_info(network_info)
+    network_info_export_to_sbml_model = sbmlplot.NetworkInfoExportToSBMLModel()
+    network_info_export_to_sbml_model.extract_graph_info(network_info_import_from_network_editor)
+    network_info_export_to_sbml_model.export(file_name)
--- test-app/src/plugins/negui_import_plugin.py
+++ test-app/src/plugins/negui_import_plugin.py
@@ -29,9 +29,9 @@ def loadJson(filename):
     
 
 def loadSBML(file_name):
-    sbml_graph_info = sbmlplot.SBMLGraphInfoImportFromSBMLModel()
-    sbml_graph_info.extract_info(file_name)
-    network_editor_json = sbmlplot.SBMLGraphInfoExportToNetworkEditor()
-    network_editor_json.extract_graph_info(sbml_graph_info)
-    graph_info = network_editor_json.export()
-    return json.dumps(graph_info)
+    network_info_import_from_sbml_model = sbmlplot.NetworkInfoImportFromSBMLModel()
+    network_info_import_from_sbml_model.extract_info(file_name)
+    network_info_export_to_network_editor = sbmlplot.NetworkInfoExportToNetworkEditor()
+    network_info_export_to_network_editor.extract_graph_info(network_info_import_from_sbml_model)
+    network_info = network_info_export_to_network_editor.get_graph_info(file_name)
+    return json.dumps(network_info)
