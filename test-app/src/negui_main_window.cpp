26a95a5fc649baba00a2a68a6e56fd4045d4cbaf
adelhpour <>
2023-08-07 19:15:25 -0700

    main window is subclassed and customized

---
+++ test-app/src/negui_main_window.cpp
@@ -0,0 +1,17 @@
+#include "negui_main_window.h"
+#include "negui_c_api.h"
+#include "negui_menu_bar.h"
+
+
+MyMainWindow::MyMainWindow(QWidget* parent) : QMainWindow(parent) {
+    QWidget* networkEditorWidget = createNetworkEditorWidget(this);
+    setCentralWidget(networkEditorWidget);
+    setGeometry(networkEditorWidget->geometry());
+    setMenuBar();
+}
+
+void MyMainWindow::setMenuBar() {
+    QMenuBar* menuBar = new MyMenuBar();
+    QMainWindow::setMenuBar(menuBar);
+    connect(menuBar, SIGNAL(askForSetNewNetworkCanvas()), centralWidget(), SIGNAL(askForSetNewNetworkCanvas()));
+}
---
+++ test-app/src/negui_main_window.h
@@ -0,0 +1,16 @@
+#ifndef __NEGUI_MAIN_WINDOW_H
+#define __NEGUI_MAIN_WINDOW_H
+
+#include <QMainWindow>
+
+class MyMainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+
+    MyMainWindow(QWidget* parent = nullptr);
+
+    void setMenuBar();
+};
+
+#endif
