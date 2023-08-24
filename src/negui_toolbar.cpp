3aff1049dea643c289e479af8a04778c3af22455
adelhpour <>
2023-08-24 16:08:55 -0700

    the size of toolbar icons are set

--- src/negui_toolbar.cpp
+++ src/negui_toolbar.cpp
@@ -5,6 +5,7 @@
 MyToolBar::MyToolBar(QWidget* parent) : QToolBar(parent) {
     setStyleSheet("QToolBar {background-color : white; border: 1px solid lightgray;  border-radius: 5px;}");
     setContentsMargins(0, 0, 0, 0);
+    setIconSize(QSize(45, 45));
 };
 
 void MyToolBar::setButtons(QList<QAbstractButton*> buttons) {
