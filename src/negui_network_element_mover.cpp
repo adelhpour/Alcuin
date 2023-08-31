0fa60bc5d33520faa78d7bc8713dceadcdf8a270
adelhpour <>
2023-08-30 19:24:22 -0700

    element mover class is created

---
+++ src/negui_network_element_mover.cpp
@@ -0,0 +1,19 @@
+#include "negui_network_element_mover.h"
+#include "negui_node.h"
+
+// MyNetworkElementMoverBase
+
+MyNetworkElementMoverBase::MyNetworkElementMoverBase(QList<MyNetworkElementBase*> networkElements, MyNetworkElementBase* movedNetworkElement) {
+    _networkElements = networkElements;
+    _movedNetworkElement = movedNetworkElement;
+}
+
+// MyNodeMover
+
+MyNodeMover::MyNodeMover(QList<MyNetworkElementBase*> networkElements, MyNetworkElementBase* movedNetworkElement) : MyNetworkElementMoverBase(networkElements, movedNetworkElement) {
+
+}
+
+void MyNodeMover::move(const qreal& dx, const qreal& dy) {
+
+}
