6d5bd8300fcd2b2752ad1bee18da289e1deabadc
adelhpour <>
2023-09-22 13:05:09 -0700

    The initial contents of the multi element feature menu is designed
    - border-color parameter is added to this menu if the network elements passing into it have this parameter in their shape styles
    
    - fill-color parameter is added to this menu if the network elements passing into it have this parameter in their shape styles

--- src/negui_multi_network_element_feature_menu.cpp
+++ src/negui_multi_network_element_feature_menu.cpp
@@ -4,13 +4,88 @@
 
 // MyMultiNetworkElementFeatureMenu
 
-MyMultiNetworkElementFeatureMenu::MyMultiNetworkElementFeatureMenu(const QString& iconsDirectoryPath, QWidget *parent) : MyFeatureMenuBase(iconsDirectoryPath, parent) {
+MyMultiNetworkElementFeatureMenu::MyMultiNetworkElementFeatureMenu(QList<MyNetworkElementBase*> networkElements, const QString& iconsDirectoryPath, QWidget *parent) : MyFeatureMenuBase(iconsDirectoryPath, parent) {
+    _networkElements = networkElements;
     QGridLayout* contentLayout = (QGridLayout*)layout();
     contentLayout->addWidget(new MyTitleLabel("Menu"), contentLayout->rowCount(), 0, 1, 2, Qt::AlignHCenter | Qt::AlignTop);
+    MyParameterBase* parameter = NULL;
+    MyParameterBase* representativeBorderColorParameter = createRepresentativeBorderColorParameter();
+    if (representativeBorderColorParameter) {
+        contentLayout->addWidget(new MyLabel((representativeBorderColorParameter->name()), representativeBorderColorParameter->hint()), contentLayout->rowCount(), 0, Qt::AlignLeft);
+        contentLayout->addWidget(representativeBorderColorParameter->inputWidget(), contentLayout->rowCount() - 1, 1, Qt::AlignRight);
+    }
+    MyParameterBase* representativeFillColorParameter = createRepresentativeFillColorParameter();
+    if (representativeFillColorParameter) {
+        contentLayout->addWidget(new MyLabel((representativeFillColorParameter->name()), representativeFillColorParameter->hint()), contentLayout->rowCount(), 0, Qt::AlignLeft);
+        contentLayout->addWidget(representativeFillColorParameter->inputWidget(), contentLayout->rowCount() - 1, 1, Qt::AlignRight);
+    }
     contentLayout->setAlignment(Qt::AlignTop);
     setFixedWidth(340);
 }
 
 MyMultiNetworkElementFeatureMenu::FEATURE_MENU_TYPE MyMultiNetworkElementFeatureMenu::type() {
     return MULTI_NETWORK_ELEMENT_FEATURE_MENU;
+}
+
+MyParameterBase* MyMultiNetworkElementFeatureMenu::createRepresentativeBorderColorParameter() {
+    MyParameterBase* representativeBorderColorParameter = NULL;
+    if (getNetworkElementParameters("border-color").size()) {
+        representativeBorderColorParameter = new MyBorderColorParameter();
+        connect(representativeBorderColorParameter, &MyParameterBase::isUpdated, this, [this, representativeBorderColorParameter] () {
+            representativeBorderColorParameter->setDefaultValue();
+            updateBorderColorParameters(((MyBorderColorParameter*)representativeBorderColorParameter)->defaultValue());
+            updateNetworkElements();
+        });
+    }
+
+    return representativeBorderColorParameter;
+}
+
+void MyMultiNetworkElementFeatureMenu::updateBorderColorParameters(const QString& borderColor) {
+    QList<MyParameterBase*> borderColorParameters = getNetworkElementParameters("border-color");
+    for (MyParameterBase* borderColorParameter : borderColorParameters)
+        ((MyBorderColorParameter*)borderColorParameter)->setDefaultValue(borderColor);
+}
+
+MyParameterBase* MyMultiNetworkElementFeatureMenu::createRepresentativeFillColorParameter() {
+    MyParameterBase* representativeFillColorParameter = NULL;
+    if (getNetworkElementParameters("fill-color").size()) {
+        representativeFillColorParameter = new MyFillColorParameter();
+        connect(representativeFillColorParameter, &MyParameterBase::isUpdated, this, [this, representativeFillColorParameter] () {
+            representativeFillColorParameter->setDefaultValue();
+            updateFillColorParameters(((MyFillColorParameter*)representativeFillColorParameter)->defaultValue());
+            updateNetworkElements();
+        });
+    }
+
+    return representativeFillColorParameter;
+}
+
+void MyMultiNetworkElementFeatureMenu::updateFillColorParameters(const QString& fillColor) {
+    QList<MyParameterBase*> fillColorParameters = getNetworkElementParameters("fill-color");
+    for (MyParameterBase* fillColorParameter : fillColorParameters)
+        ((MyFillColorParameter*)fillColorParameter)->setDefaultValue(fillColor);
+}
+
+void MyMultiNetworkElementFeatureMenu::updateNetworkElements() {
+    for (MyNetworkElementBase* networkElement : _networkElements) {
+        networkElement->updateStyle(networkElement->style()->shapeStyles());
+        networkElement->updateGraphicsItem();
+        networkElement->updateFocusedGraphicsItems();
+    }
+    emit askForCreateChangeStageCommand();
+}
+
+QList<MyParameterBase*> MyMultiNetworkElementFeatureMenu::getNetworkElementParameters(const QString& parameterName) {
+    QList<MyParameterBase*> parameters;
+    MyParameterBase* parameter = NULL;
+    for (MyNetworkElementBase* networkElement : _networkElements) {
+        for (MyShapeStyleBase* shapeStyle : networkElement->style()->shapeStyles()) {
+            parameter = shapeStyle->findParameter(parameterName);
+            if (parameter)
+                parameters.push_back(parameter);
+        }
+    }
+
+    return parameters;
 }
--- src/negui_multi_network_element_feature_menu.h
+++ src/negui_multi_network_element_feature_menu.h
@@ -2,15 +2,35 @@
 #define __NEGUI_MULTI_ELEMENT_FEATURE_MENU_H
 
 #include "negui_feature_menu_base.h"
+#include "negui_network_element_base.h"
 
 class MyMultiNetworkElementFeatureMenu : public MyFeatureMenuBase {
     Q_OBJECT
 
 public:
 
-    MyMultiNetworkElementFeatureMenu(const QString& iconsDirectoryPath, QWidget *parent = nullptr);
+    MyMultiNetworkElementFeatureMenu(QList<MyNetworkElementBase*> networkElements, const QString& iconsDirectoryPath, QWidget *parent = nullptr);
 
     FEATURE_MENU_TYPE type() override;
+
+    MyParameterBase* createRepresentativeBorderColorParameter();
+
+    void updateBorderColorParameters(const QString& borderColor);
+
+    MyParameterBase* createRepresentativeFillColorParameter();
+
+    void updateFillColorParameters(const QString& fillColor);
+
+    void updateNetworkElements();
+
+    QList<MyParameterBase*> getNetworkElementParameters(const QString& parameterName);
+
+signals:
+
+    void askForCreateChangeStageCommand();
+
+protected:
+    QList<MyNetworkElementBase*> _networkElements;
 };
 
 #endif
