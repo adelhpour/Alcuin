a0adcf21f1150f2d4402d30d43958b5f02c58d27
adelhpour <>
2023-09-22 19:01:06 -0700

    title of the null feature menu is changed to 'No Element'

--- src/negui_null_feature_menu.cpp
+++ src/negui_null_feature_menu.cpp
@@ -6,7 +6,7 @@
 
 MyNullFeatureMenu::MyNullFeatureMenu(const QString& iconsDirectoryPath, QWidget *parent) : MyFeatureMenuBase(iconsDirectoryPath, parent) {
     QGridLayout* contentLayout = (QGridLayout*)layout();
-    contentLayout->addWidget(new MyTitleLabel("Menu"), contentLayout->rowCount(), 0, 1, 2, Qt::AlignHCenter | Qt::AlignTop);
+    contentLayout->addWidget(new MyTitleLabel("No Element"), contentLayout->rowCount(), 0, 1, 2, Qt::AlignHCenter | Qt::AlignTop);
     contentLayout->addWidget(new MyNoSelectedElementTitle(), contentLayout->rowCount(), 0, 1, 2, Qt::AlignHCenter | Qt::AlignTop);
     contentLayout->setAlignment(Qt::AlignTop);
     setFixedWidth(340);
