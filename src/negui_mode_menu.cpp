2ea1c24965e6ae58ea4f647bfe9ed1125ee9d296
adelhpour <>
2023-10-10 13:32:15 -0700

    the derivative classes of mode menu base are now deprecated and it is renamed to mode menu

--- src/negui_mode_menu.cpp
+++ src/negui_mode_menu.cpp
@@ -1,54 +1,28 @@
 #include "negui_mode_menu.h"
-#include "negui_decorate_menu_buttons.h"
-#include "negui_customized_mode_menu_widgets.h"
+#include "negui_customized_menu_button_widgets.h"
 
 #include <QGridLayout>
 
-// MyModeMenuBase
+// MyModeMenu
 
-MyModeMenuBase::MyModeMenuBase(QWidget *parent) : MyFrame(parent) {
+MyModeMenu::MyModeMenu(QWidget *parent) : MyFrame(parent) {
     QGridLayout* contentLayout = new QGridLayout(this);
     setLayout(contentLayout);
     setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
     setFixedSize(QSize(0, 0));
 }
 
-void MyModeMenuBase::setNormalModeButton(QAbstractButton* button, const QString& iconsDirectoryPath) {
-    QGridLayout *contentLayout = (QGridLayout *) layout();
-    decorateNormalModeButton(button, iconsDirectoryPath);
-    contentLayout->addWidget(button, contentLayout->rowCount(), 0);
-    _buttons.push_back(button);
-    button->setFixedSize(QSize(buttonSize(), buttonSize()));
-    setFixedSize(QSize(qMax(width(), button->width() + buttonPadding()), height() + button->height() + buttonPadding()));
-}
-
-void MyModeMenuBase::setFrequentlyUsedButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath) {
-
-}
-
-void MyModeMenuBase::setAddModeButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath) {
-
-}
-
-void MyModeMenuBase::setZoomInButton(QAbstractButton* button, const QString& iconsDirectoryPath) {
+void MyModeMenu::addButtons(QList<QAbstractButton*> buttons) {
     QGridLayout* contentLayout = (QGridLayout*)layout();
-    decorateZoomInButton(button, iconsDirectoryPath);
-    contentLayout->addWidget(button, contentLayout->rowCount(), 0);
-    _buttons.push_back(button);
-    button->setFixedSize(QSize(buttonSize(), buttonSize()));
-    setFixedSize(QSize(qMax(width(), button->width() + buttonPadding()), height() + button->height() + buttonPadding()));
+    for (QAbstractButton* button : buttons) {
+        contentLayout->addWidget(button, contentLayout->rowCount(), 0);
+        _buttons.push_back(button);
+        button->setFixedSize(QSize(buttonSize(), buttonSize()));
+        setFixedSize(QSize(qMax(width(), button->width() + buttonPadding()), height() + button->height() + buttonPadding()));
+    }
 }
 
-void MyModeMenuBase::setZoomOutButton(QAbstractButton* button, const QString& iconsDirectoryPath) {
-    QGridLayout *contentLayout = (QGridLayout *) layout();
-    decorateZoomOutButton(button, iconsDirectoryPath);
-    contentLayout->addWidget(button, contentLayout->rowCount(), 0);
-    _buttons.push_back(button);
-    button->setFixedSize(QSize(buttonSize(), buttonSize()));
-    setFixedSize(QSize(qMax(width(), button->width() + buttonPadding()), height() + button->height() + buttonPadding()));
-}
-
-void MyModeMenuBase::setMode(const QString& mode) {
+void MyModeMenu::setMode(const QString& mode) {
     deactivateButtons();
     for (QAbstractButton* button : getButtons()) {
         if (isModeToolButton(button)) {
@@ -60,11 +34,11 @@ void MyModeMenuBase::setMode(const QString& mode) {
     }
 }
 
-QList<QAbstractButton*> MyModeMenuBase::getButtons() {
+QList<QAbstractButton*> MyModeMenu::getButtons() {
     return _buttons;
 }
 
-void MyModeMenuBase::activateAddElementButton(const QString& elementStyleName) {
+void MyModeMenu::activateAddElementButton(const QString& elementStyleName) {
     deactivateButtons();
     for (QAbstractButton* button : getButtons()) {
         if (button->toolTip() == elementStyleName)
@@ -72,49 +46,19 @@ void MyModeMenuBase::activateAddElementButton(const QString& elementStyleName) {
     }
 }
 
-void MyModeMenuBase::deactivateButtons() {
+void MyModeMenu::deactivateButtons() {
     for (QAbstractButton* button : getButtons()) {
-        if (dynamic_cast<MyModeMenuToolButton*>(button))
-            ((MyModeMenuToolButton*)button)->setActive(false);
+        if (dynamic_cast<MyModeMenuButton*>(button))
+            ((MyModeMenuButton*)button)->setActive(false);
     }
 }
 
-qint32 MyModeMenuBase::buttonSize() {
+qint32 MyModeMenu::buttonSize() {
     return 45.0;
 }
 
-qint32 MyModeMenuBase::buttonPadding() {
+qint32 MyModeMenu::buttonPadding() {
     return buttonSize() / 3;
-}
-
-// MyFrequentlyUsedButtonsModeMenu
-
-MyFrequentlyUsedButtonsModeMenu::MyFrequentlyUsedButtonsModeMenu(QWidget *parent) : MyModeMenuBase(parent) {
-
-}
-
-void MyFrequentlyUsedButtonsModeMenu::setFrequentlyUsedButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath) {
-    QGridLayout* contentLayout = (QGridLayout*)layout();
-    for (QAbstractButton* button : buttons) {
-        contentLayout->addWidget(button, contentLayout->rowCount(), 0);
-        _buttons.push_back(button);
-        button->setFixedSize(QSize(buttonSize(), buttonSize()));
-        setFixedSize(QSize(qMax(width(), button->width() + buttonPadding()), height() + button->height() + buttonPadding()));
-    }
-}
-
-// MyAddModeButtonsModeMenu
-
-MyAddModeButtonsModeMenu::MyAddModeButtonsModeMenu(QWidget *parent) : MyModeMenuBase(parent) {
-
-}
-
-void MyAddModeButtonsModeMenu::setAddModeButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath) {
-    QGridLayout* contentLayout = (QGridLayout*)layout();
-    QAbstractButton* button = decorateAddModeButton(buttons, iconsDirectoryPath);
-    contentLayout->addWidget(button, contentLayout->rowCount(), 0);
-    _buttons.push_back(button);
-    setFixedSize(QSize(qMax(width(), button->width() + buttonPadding()), height() + button->height() + buttonPadding()));
 }
 
 const bool isModeToolButton(QAbstractButton* button) {
--- src/negui_mode_menu.h
+++ src/negui_mode_menu.h
@@ -3,22 +3,14 @@
 
 #include "negui_customized_common_widgets.h"
 
-class MyModeMenuBase : public MyFrame {
+class MyModeMenu : public MyFrame {
     Q_OBJECT
 
 public:
 
-    MyModeMenuBase(QWidget *parent = nullptr);
+    MyModeMenu(QWidget *parent = nullptr);
 
-    void setNormalModeButton(QAbstractButton* button, const QString& iconsDirectoryPath);
-
-    virtual void setFrequentlyUsedButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath);
-
-    virtual void setAddModeButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath);
-
-    void setZoomInButton(QAbstractButton* button, const QString& iconsDirectoryPath);
-
-    void setZoomOutButton(QAbstractButton* button, const QString& iconsDirectoryPath);
+    void addButtons(QList<QAbstractButton*> buttons);
 
     QList<QAbstractButton*> getButtons();
 
@@ -28,7 +20,7 @@ public:
 
 public slots:
 
-    void setMode(const QString& mode);
+            void setMode(const QString& mode);
 
     void deactivateButtons();
 
@@ -38,27 +30,6 @@ protected:
     QList<QAbstractButton*> _buttons;
 };
 
-class MyFrequentlyUsedButtonsModeMenu : public MyModeMenuBase {
-    Q_OBJECT
-
-public:
-
-    MyFrequentlyUsedButtonsModeMenu(QWidget *parent = nullptr);
-
-    void setFrequentlyUsedButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath) override;
-};
-
-class MyAddModeButtonsModeMenu : public MyModeMenuBase {
-    Q_OBJECT
-
-public:
-
-    MyAddModeButtonsModeMenu(QWidget *parent = nullptr);
-
-    void setAddModeButtons(QList<QAbstractButton*> buttons, const QString& iconsDirectoryPath) override;
-};
-
 const bool isModeToolButton(QAbstractButton* button);
 
 #endif
-
